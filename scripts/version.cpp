4f003bac366ab7d896d3513ffca99a26dcb28791
soonghee2 <soonghee2@ajou.ac.kr>
2025-01-26 10:01:06 +0900

    Create version.cpp

---
+++ scripts/version.cpp
@@ -0,0 +1,25 @@
+//송희 과제 때문에 잠시만 추가하겠습니다..
+
+#include <stdio.h>
+
+#ifdef linux
+int JenkinsBuilderFileVersion[4] attribute((section ("FileVersion"))) = { 2,0,2,51 };
+int JenkinsBuilderProductVersion[4] attribute((section ("ProductVersion"))) = { 2024,11,25,51 };
+#endif
+
+int main(int nArgc, char** pszArgv)
+{
+    printf("FileVersion: %d.%d.%d.%d\n",
+        JenkinsBuilderFileVersion[0],
+        JenkinsBuilderFileVersion[1],
+        JenkinsBuilderFileVersion[2],
+        JenkinsBuilderFileVersion[3]
+        );
+    printf("ProductVersion: %d.%d.%d.%d\n",
+        JenkinsBuilderProductVersion[0],
+        JenkinsBuilderProductVersion[1],
+        JenkinsBuilderProductVersion[2],
+        JenkinsBuilderProductVersion[3]
+        );
+    return 0;
+}
